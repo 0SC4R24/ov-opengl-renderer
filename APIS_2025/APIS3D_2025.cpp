8bbc6f857bc6c1e52c32332a8e82f71804828007
0SC4R24 <oscarviudez24@gmail.com>
2026-01-31 09:29:38 +0100

    refactor: object creation in main
    Replaces the separate creation of the TrianguloRot object with an inline call to std::make_shared when adding the object to the system.

--- APIS_2025/APIS3D_2025.cpp
+++ APIS_2025/APIS3D_2025.cpp
@@ -8,13 +8,7 @@
 int main(int argc, char** argv)
 {
     System::initSystem(MO_RENDER_ENGINE_TYPE_GL4, MO_INPUT_MANAGER_TYPE_GLFW);
-
-    /*old::Camera cam(glm::vec4(0, 0, 3, 1), glm::vec4(0, 0, 0, 1));
-    System::getRender()->setCamera(&cam);*/
-
-    auto triangulo = std::make_shared<TrianguloRot>();
-    System::addObject(triangulo);
-
+    System::addObject(std::make_shared<TrianguloRot>());
     System::mainLoop();
 
 	return 0;
