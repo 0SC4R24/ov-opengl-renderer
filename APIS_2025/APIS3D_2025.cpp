afa70629d2d54732d0c0c993ce90dc063762ce49
0SC4R24 <oscarviudez24@gmail.com>
2026-04-13 03:14:56 +0200

    feat: refactor main function and add new rendering functions for 3D objects

--- APIS_2025/APIS3D_2025.cpp
+++ APIS_2025/APIS3D_2025.cpp
@@ -9,62 +9,145 @@
 #include "ov/CubeTex.h"
 #include "ov/OrbitalLight.h"
 
+void feng_shui()
+{
+	auto camera = std::make_shared<CameraFPS>(
+		OV_PROJECTION_TYPE_PERSPECTIVE,        // tipo
+		glm::vec3(0.6f, 1.0f, 0.9f),     // posicion
+		glm::vec3(0, 1, 0),              // up
+		glm::vec3(-0.54f, -0.93f, -1.0f) // lookAt
+	);
+    
+	System::getWorld()->addCamera(camera);
+
+	auto center = glm::vec4(0);
+	float radius = 10.0f;
+	float speed = 2.0f;
+
+	System::getWorld()->setAmbient(0.2f);
+    
+	auto light = std::make_shared<OrbitalLight>(
+		OV_LIGHT_TYPE_POINT,
+		glm::vec4(-10, 0, 0, 1),
+		glm::vec4(1, 0, 0, 1),
+		glm::vec4(0, -1, 0, 0),
+		center,
+		radius,
+		speed,
+		0.2f
+	);
+    
+	auto light2 = std::make_shared<Light>(
+		OV_LIGHT_TYPE_DIRECTIONAL,
+		glm::vec4(0, 0, 0, 1),
+		glm::vec4(1, 1, 1, 1),
+		glm::vec4(0, 1, -1, 0)
+	);
+    
+	System::getWorld()->addLight(light);
+	System::getWorld()->addLight(light2);
+    
+	auto feng_shui = std::make_shared<Object3D>();
+	feng_shui->loadDataFromMshFile("data/feng_shui/Feng_Shui.msh");
+	feng_shui->setScale(glm::vec4(0.005f));
+    
+	System::addObject(feng_shui);
+	
+	System::mainLoop();
+}
+
+void tiki()
+{
+	auto camera = std::make_shared<CameraFPS>(
+		OV_PROJECTION_TYPE_PERSPECTIVE,        // tipo
+		glm::vec3(0.6f, 1.0f, 0.9f),     // posicion
+		glm::vec3(0, 1, 0),              // up
+		glm::vec3(-0.54f, -0.93f, -1.0f) // lookAt
+	);
+    
+	System::getWorld()->addCamera(camera);
+	
+	auto center = glm::vec4(0);
+	float radius = 10.0f;
+	float speed = 2.0f;
+
+	System::getWorld()->setAmbient(0.2f);
+    
+	auto light = std::make_shared<OrbitalLight>(
+		OV_LIGHT_TYPE_POINT,
+		glm::vec4(-10, 0, 0, 1),
+		glm::vec4(1, 0, 0, 1),
+		glm::vec4(0, -1, 0, 0),
+		center,
+		radius,
+		speed,
+		0.2f
+	);
+    
+	auto light2 = std::make_shared<Light>(
+		OV_LIGHT_TYPE_DIRECTIONAL,
+		glm::vec4(0, 0, 0, 1),
+		glm::vec4(1, 1, 1, 1),
+		glm::vec4(0, 1, -1, 0)
+	);
+    
+	System::getWorld()->addLight(light);
+	System::getWorld()->addLight(light2);
+	
+	auto tiki = std::make_shared<Object3D>();
+	tiki->loadDataFromMshFile("data/tiki/tiki.msh");
+	
+	System::addObject(tiki);
+	
+	System::mainLoop();
+}
+
+void textured_cube()
+{
+	auto camera = std::make_shared<CameraFPS>(
+		OV_PROJECTION_TYPE_PERSPECTIVE,        // tipo
+		glm::vec3(0.6f, 1.0f, 0.9f),     // posicion
+		glm::vec3(0, 1, 0),              // up
+		glm::vec3(-0.54f, -0.93f, -1.0f) // lookAt
+	);
+    
+	System::getWorld()->addCamera(camera);
+	
+	auto cube = std::make_shared<CubeTex>();
+	
+	System::addObject(cube);
+	
+	System::mainLoop();
+}
+
+void asian_town()
+{
+	auto camera = std::make_shared<CameraFPS>(
+		OV_PROJECTION_TYPE_PERSPECTIVE,        // tipo
+		glm::vec3(0, 0.5f, 2),           // posicion
+		glm::vec3(0, 1, 0),              // up
+		glm::vec3(0, 0, 0)               // lookAt
+	);
+    
+	System::getWorld()->addCamera(camera);
+
+	//auto cube = std::make_shared<CubeTex>();
+
+	auto asianTown = std::make_shared<Object3D>();
+	asianTown->loadDataFromMshFile("data/asian_town.msh");
+	asianTown->setScale(glm::vec4(5.0f, 5.0f, 5.0f, 1));
+	
+	System::addObject(asianTown);
+    
+	System::mainLoop();
+}
+
 int main(int argc, char** argv)
 {
-    System::setParameters("OV OpenGL Renderer v6.0", 640, 480);
+    System::setParameters("OV OpenGL Renderer v5.0", 640, 480);
     System::initSystem(OV_RENDER_ENGINE_TYPE_GL4, OV_INPUT_MANAGER_TYPE_GLFW);
-
-    auto camera = std::make_shared<CameraFPS>(
-        OV_PROJECTION_TYPE_PERSPECTIVE,        // tipo
-        glm::vec3(0.6f, 1.0f, 0.9f),     // posicion
-        glm::vec3(0, 1, 0),              // up
-        glm::vec3(-0.54f, -0.93f, -1.0f) // lookAt
-    );
     
-    System::getWorld()->addCamera(camera);
-
-    auto cube = std::make_shared<CubeTex>();
-    auto center = glm::vec4(0);
-    float radius = 10.0f;
-    float speed = 2.0f;
-
-    System::getWorld()->setAmbient(0.2f);
-    
-    auto light = std::make_shared<OrbitalLight>(
-        OV_LIGHT_TYPE_POINT,
-        glm::vec4(-10, 0, 0, 1),
-        glm::vec4(1, 0, 0, 1),
-        glm::vec4(0, -1, 0, 0),
-        center,
-        radius,
-        speed,
-        0.2f
-    );
-    
-    auto light2 = std::make_shared<Light>(
-        OV_LIGHT_TYPE_DIRECTIONAL,
-        glm::vec4(0, 0, 0, 1),
-        glm::vec4(1, 1, 1, 1),
-        glm::vec4(0, 1, -1, 0)
-    );
-    
-    System::getWorld()->addLight(light);
-    System::getWorld()->addLight(light2);
-    
-    // auto feng_shui = std::make_shared<Object3D>();
-    // feng_shui->loadDataFromMshFile("data/feng_shui/Feng_Shui.msh");
-    // feng_shui->setScale(glm::vec4(0.005f));
-    //
-    // System::addObject(feng_shui);
-    
-    // auto tiki = std::make_shared<Object3D>();
-    // tiki->loadDataFromMshFile("data/tiki/tiki.msh");
-    //
-    // System::addObject(tiki);
-    
-    System::addObject(cube);
-    
-    System::mainLoop();
+    textured_cube();
 
 	return 0;
 }
