52998c94b90d0c8506281efa1e814465a9c4b062
0SC4R24 <oscarviudez24@gmail.com>
2026-04-14 03:22:44 +0200

    feat: add Billboard class interface for 3D rendering

---
+++ APIS_2025/src/ov/Billboard.h
@@ -0,0 +1,34 @@
+﻿///
+/// OV-OpenGL-Renderer
+/// Oscar Viudez
+/// Miguel Gutierrez
+/// 2026
+/// 
+
+#pragma once
+
+#include "common.h"
+#include "Object.h"
+
+/// <summary>
+/// Billboard Interface
+/// </summary>
+class Billboard : Object
+{
+protected: // Attributes
+    float m_spin;
+    
+public: // Constructor
+    Billboard();
+    
+public: // Getters & Setters
+    float getSpin() const
+    { return m_spin; }
+    
+    void setSpin(float spin)
+    { m_spin = spin; }
+    
+public: // Virtual Methods
+    virtual void computeModelMatrix() override;
+    virtual void step(double deltaTime) = 0;
+};
