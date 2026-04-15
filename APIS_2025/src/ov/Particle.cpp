2a3ecdfceae1a44c89b0229b2bf6ac08ef77de1d
0SC4R24 <oscarviudez24@gmail.com>
2026-04-15 03:53:23 +0200

    feat: add Particle class for particle system implementation

---
+++ APIS_2025/src/ov/Particle.cpp
@@ -0,0 +1,14 @@
+﻿#include "Particle.h"
+
+Particle::Particle(std::string mshFile, const glm::vec3& velocity, float spinVelocity, double lifeTime, bool autoFade)
+    : Billboard(mshFile, spinVelocity)
+{
+    m_velocity = glm::vec4(velocity,0 );
+    m_lifeTime = lifeTime;
+    m_autoFade = autoFade;
+}
+
+float Particle::getRemainingLifetime() const
+{
+    return m_lifeTime;
+}
---
+++ APIS_2025/src/ov/Particle.h
@@ -0,0 +1,27 @@
+﻿///
+/// OV-OpenGL-Renderer
+/// Oscar Viudez
+/// Miguel Gutierrez
+/// 2026
+/// 
+
+#pragma once
+
+#include "Billboard.h"
+
+class Particle : public Billboard
+{
+private: // Attributes
+    glm::vec4 m_velocity;
+    double m_lifeTime;
+    bool m_autoFade;
+    
+public: // Constructor
+    Particle(std::string mshFile, const glm::vec3& velocity, float spinVelocity, double lifeTime, bool autoFade);
+    
+public: // Methods
+    float getRemainingLifetime() const;
+    
+public: // Virtual Methods
+    virtual void step(double deltaTime) override;
+};
